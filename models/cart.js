0f37b7b34d2b0a55a45fc09ea68d9399cb360476
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-05 17:17:13 +0530

    add product to cart feature

---
+++ models/cart.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const fs = require("fs");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    console.log("ADd product called");
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProductIndex = cart.products.findIndex(
+        (product) => product.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      if (existingProduct) {
+        console.log("if entered");
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+        console.log(cart);
+      } else {
+        console.log("else entered");
+        updatedProduct = {
+          id: id,
+          qty: 1,
+        };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      console.log("near writeFile");
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
