cb1183835e55d1a9457cb75301809f13df689e30
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-26 02:00:31 +0530

    delete item from cart

--- models/user.js
+++ models/user.js
@@ -64,6 +64,14 @@ userSchema.methods.getCart = function () {
     .catch((err) => console.error(err));
 };
 
+userSchema.methods.deleteItemFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(
+    (item) => item.productId.toString() !== productId.toString()
+  );
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
 
 // const { getDb } = require("../util/database");
