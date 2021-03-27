bbcf3ebf863c4646354fc35ee14e649242b5b6a8
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-27 19:36:07 +0530

    added sign up page

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,9 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
+router.get("/signup", authController.getSignUp);
 router.post("/login", authController.postLogin);
+router.post("/signup", authController.postSignUp);
 router.post("/logout", authController.postLogout);
 
 module.exports = router;
