299089ddfe0f586045a750a6f9c66cbace1dd31b
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-28 23:17:34 +0530

    added validations for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendInBlueTransport = require("nodemailer-sendinblue-transport");
 const { reset } = require("nodemon");
+const { validationResult } = require("express-validator/check");
 const User = require("../models/user");
 
 const transporter = nodemailer.createTransport(
@@ -73,6 +74,14 @@ exports.postSignUp = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   User.findOne({ email: email })
     .then((userDoc) => {
@@ -153,7 +162,7 @@ exports.postReset = (req, res, next) => {
           `,
         });
       })
-      .catch((err) => console.error(err));
+      .catch((error) => console.error(error));
   });
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, body } = require("express-validator/check");
 const authController = require("../controllers/auth");
 const router = express.Router();
 
@@ -8,8 +9,34 @@ router.get("/reset/:token", authController.getNewPassword);
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
 router.post("/login", authController.postLogin);
-router.post("/new-password", authController.postNewPassword);
-router.post("/signup", authController.postSignUp);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address is forbidden");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with alphabets and numbers and minimum length of 5"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignUp
+);
 router.post("/logout", authController.postLogout);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
