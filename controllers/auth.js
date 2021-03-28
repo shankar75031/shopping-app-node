98a6d046314a575fcfbd9e2f8fdcdb305577b8bb
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-28 21:56:15 +0530

    send password reset link and store reset token in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendInBlueTransport = require("nodemailer-sendinblue-transport");
 const User = require("../models/user");
@@ -106,5 +107,51 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found!");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        return transporter.sendMail({
+          to: req.body.email,
+          from: "shankar75031@gmail.com",
+          subject: "Password Reset",
+          html: `
+          <h1>You requested a password reset</h1>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">LINK</a> to reset password.</p>
+          `,
+        });
+      })
+      .catch((err) => console.error(err));
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignUp);
+router.get("/reset", authController.getReset);
+router.post("/reset", authController.postReset);
 router.post("/login", authController.postLogin);
 router.post("/signup", authController.postSignUp);
 router.post("/logout", authController.postLogout);
