ae1a3b6e877d1398534838997ceed9ab5c807f82
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-29 20:59:59 +0530

    added images

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const session = require("express-session");
 const csrf = require("csurf");
 const flash = require("connect-flash");
+const hash = require("random-hash");
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -25,10 +26,11 @@ const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
+    // cb(null, path.join(__dirname, "images"));
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(null, hash.generateHash({ length: 5 }) + "-" + file.originalname);
   },
 });
 
@@ -99,6 +101,7 @@ app.use(errorController.get404);
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
+  console.log(error);
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
--- controllers/admin.js
+++ controllers/admin.js
@@ -38,7 +38,6 @@ exports.postAddProduct = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
@@ -58,7 +57,6 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = image.path;
 
   const product = new Product({
-    // _id: new mongoose.Types.ObjectId('5badf72403fd8b5be0366e81'),
     title: title,
     price: price,
     description: description,
@@ -68,26 +66,10 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then((result) => {
-      // console.log(result);
       console.log("Created Product");
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      // return res.status(500).render('admin/edit-product', {
-      //   pageTitle: 'Add Product',
-      //   path: '/admin/add-product',
-      //   editing: false,
-      //   hasError: true,
-      //   product: {
-      //     title: title,
-      //     imageUrl: imageUrl,
-      //     price: price,
-      //     description: description
-      //   },
-      //   errorMessage: 'Database operation failed, please try again.',
-      //   validationErrors: []
-      // });
-      // res.redirect('/500');
       const error = new Error(err);
       error.httpStatusCode = 500;
       error.isLoggedIn = req.session.isLoggedIn;
